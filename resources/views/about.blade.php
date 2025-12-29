81217f1cb332ba678e4d0d49e956f08c7cd08441
Razib Hasan <razibhasan2403@gmail.com>
2025-12-29 13:08:06 +0600

    Simple Works Done 29-12-25

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+@extends("layouts.default")
+
+@section("content")
+
+<div class="container-fluid bg-3 text-center">
+    <h1>About Page</h1><br>
+    <div class="row">
+        <div class="col-sm-12">
+            <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio iste quia incidunt, quibusdam eaque quod saepe laboriosam vel minus eum voluptatibus quam, officia modi repellendus nemo voluptate architecto placeat mollitia.</p>
+
+        </div>
+    </div>
+</div><br>
+
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends ("layouts.default")
+
+
+@section ("content")
+
+<div class="container-fluid bg-3 text-center">
+<h1>Contact Page</h1>
+
+
+@endsection
